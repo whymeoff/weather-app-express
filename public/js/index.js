8a5ebd6a0aa27ff5086a9a1d571c5d974598e6d7
Mark <50715288+whymeoff@users.noreply.github.com>
2020-01-08 23:56:35 +0200

    Heroku deployment

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ weatherForm.addEventListener('submit', (e) => {
     
     const address = document.querySelector('#search-input').value;
     const out = document.querySelector('#out');
-    fetch(`http://localhost:3000/weather?address=${address}`).then((res) => {
+    fetch(`/weather?address=${address}`).then((res) => {
         res.json().then((data) => {
             if (data.error) {
                 out.innerHTML = data.error;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('../utils/forecast');
 const geoLocation = require('../utils/geoLocation');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define path for express config
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -82,6 +83,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up!');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
 })
