cf7fbe426db6277af6c95abe6adacf1c5eccac30
Mark <50715288+whymeoff@users.noreply.github.com>
2020-01-11 17:12:43 +0200

    Some fixes

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,15 @@
 const weatherForm = document.querySelector('form');
 
-weatherForm.addEventListener('submit', (e) => {
+weatherForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     
     const address = document.querySelector('#search-input').value;
     const out = document.querySelector('#out');
-    fetch(`/weather?address=${address}`).then((res) => {
-        res.json().then((data) => {
-            if (data.error) {
-                out.innerHTML = data.error;
-            } else {
-                out.innerHTML = `Your location is: ${data.timezone}. Temperature now is: ${data.temp}. Max temperature is: ${data.maxTemp}. The lowest temperature is: ${data.lowTemp}. The chance of rain is: ${data.precipProbability}%.`;
-            }
-        })
-    })
-    return false;
+    try {
+        let data = await fetch(`/weather?address=${address}`)
+        data = await data.json()
+        out.innerHTML = `Your location is: ${data.timezone}. Temperature now is: ${Math.round(data.temp)}. Max temperature is: ${Math.round(data.maxTemp)}. The lowest temperature is: ${Math.round(data.lowTemp)}. The chance of rain is: ${Math.floor(data.precipProbability)}%.`
+    } catch (e) {
+        out.innerHTML = e
+    }
 }) 
